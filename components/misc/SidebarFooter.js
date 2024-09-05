7ea3a69720f105ef3c82dd17ba19380d7f59404d
THE NAME IS SATYA <satyaKudupudi8500@gmail.com>
2024-09-05 10:05:31 +0530

    Initial commit

---
+++ components/misc/SidebarFooter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import GitHubIcon from "./GitHubIcon";
+import XIcon from "./XIcon";
+
+const SidebarFooter = () => {
+  return (
+    <div className={`z-40 flex flex-col px-6 gap-y-2`}>
+      <article className="flex gap-x-2 items-center">
+        <GitHubIcon />
+        <XIcon />
+      </article>
+
+      <p className="text-xs text-white dark:text-white mb-0">
+        &copy; {new Date().getFullYear()} Dan Cranney
+      </p>
+
+      <div className="flex text-xs text-white dark:text-white">
+        <a
+          href="https://x.com/Satya80084?t=Yhjloxa2LlqgiLPbnM8HiQ&s=09"
+          rel="noreferrer"
+          target="_blank"
+          className="flex mr-1.5 opacity-70 hover:opacity-100 text-white hover:text-white transition-all duration-150 ease-in-out"
+        >
+          Follow me
+        </a>{" "}
+        {" "}
+        
+      </div>
+    </div>
+  );
+};
+
+export default SidebarFooter;
